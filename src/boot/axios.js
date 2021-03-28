04f984b46e37d7cab4f50cd6c8b55cd38b1b44af
TuxPK <marcosxr8@gmail.com>
2021-03-28 10:04:17 -0300

    Added backend api and github access settings
    Starting the integration of the frontend with the backend.

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,19 @@
-import Vue from 'vue'
-import axios from 'axios'
+import axios from 'axios';
 
-Vue.prototype.$axios = axios
+export default ({ Vue }) => {
+  axios.interceptors.request.use((config) => {
+    const urls = config.url.split('|');
+
+    if (urls[0] === 'api') {
+      config.url = `http://localhost:3333/${urls[1]}`;
+    }
+    if (urls[0] === 'github') {
+      config.url = `https://github.com/${urls[1]}`;
+      config.headers.Authorization = localStorage.getItem('git_token');
+    }
+
+    return config;
+  });
+
+  Vue.prototype.$axios = axios;
+};
