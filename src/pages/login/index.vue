a8351dc30d180c2b4b421b91a356c1dbcbbc4a2e
TuxPK <marcosxr8@gmail.com>
2021-03-28 09:20:47 -0300

    Created LoginLayout and login page
    Starting github login by creating a layout page.

---
+++ src/pages/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="bg-grey-3">
+    <div class="row flex flex-center" style="min-height: 100vh">
+      <div class="col-sm-10 col-xs-8 col-md-3 flex-center">
+        <div class="row flex-center q-mb-lg">
+          <q-card class="bg-grey-10 q-pa-md" style="font-size: xx-large; color: white;">
+            GitStars ✨
+          </q-card>
+        </div>
+        <q-card class="q-py-xl bg-grey-2">
+          <div class="row flex-center">
+            <q-btn
+              class="bg-grey-10 flex-center"
+              type="a"
+              color="white"
+              label="Github"
+              icon="fab fa-github"
+              size="large"
+              :loading="githubValidationLoading"
+              @click="githubValidationLoading = true"
+            >
+            </q-btn>
+          </div>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      githubValidationLoading: false,
+    };
+  },
+};
+</script>
