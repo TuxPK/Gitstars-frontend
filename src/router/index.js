47689b9ab5bed0005267afe9f7e2392778641683
TuxPK <marcosxr8@gmail.com>
2021-03-28 12:06:10 -0300

    Added function to get and set github token
    Only if to query code is not empty the function will call backend
    api and set the response token at headers.

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,58 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
 
-import routes from './routes'
+import routes from './routes';
 
-Vue.use(VueRouter)
-
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
+Vue.use(VueRouter);
 
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
 
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  })
+    base: process.env.VUE_ROUTER_BASE,
+  });
 
-  return Router
+  Router.beforeEach(async (to, from, next) => {
+    const GitAuthCode = to.query.code;
+
+    await setGithubToken(GitAuthCode);
+
+    const isAuth = await isAuthenticated();
+
+    const path = getToPath(to, isAuth);
+
+    return next({ path });
+  });
+
+  return Router;
+}
+
+async function setGithubToken(code) {
+  if (!code) return false;
+
+  const githubAuth = await axios.post('api|github-auth/token', { code })
+    .then()
+    .catch(() => false);
+
+  if (!githubAuth) return false;
+
+  const { token, type } = githubAuth.data;
+
+  return localStorage.setItem('git_token', `${type} ${token}`);
+}
+
+async function isAuthenticated() {
+  return !!localStorage.getItem('git_token');
+}
+
+function getToPath(to, isAuth) {
+  let { path } = to.path;
+
+  if (to.matched.some((record) => record.meta.forAuth) && isAuth) path = '/';
+  if (to.matched.some((record) => record.meta.forVisitors) && !isAuth) path = '/login';
+
+  return path;
 }
