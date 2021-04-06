fab7ee5e10e4f0924c34fdd1336974e7ab1c9dd9
TuxPK <marcosxr8@gmail.com>
2021-04-05 22:54:27 -0300

    Home page tags management
    Added function to add, edit and remove tags from repositories,
    added search tool to filter repositories with tag name.

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,6 +6,16 @@
           class="q-mb-lg q-pa-lg full-width"
           inline
         >
+          <div class="row">
+            <q-input
+                v-model="filterTag"
+                class="q-ml-sm"
+                clearable
+                label="Buscar tags"
+                color="green"
+              >
+              </q-input>
+          </div>
         </q-card>
         <q-card
           v-for="repository in repositories"
@@ -47,9 +57,109 @@
               </div>
             </div>
           </q-card-section>
+          <q-card-actions>
+            <q-btn
+              dense
+              flat
+              label="Adicionar Tag"
+              class="q-ml-md q-pa-sm bg-blue"
+              color="white"
+              icon="fas fa-plus"
+              :loading="tags.creating_tags || tags.updating_tags"
+              @click="showTagField(repository.id)"
+            />
+          </q-card-actions>
+          <q-card-section>
+            <div class="row">
+              <q-chip
+                square
+                v-for="tag of getRepositoryTags(repository.id)"
+                :key="tag.uuid"
+              >
+                {{ tag.name }}
+              </q-chip>
+            </div>
+          </q-card-section>
         </q-card>
       </div>
     </div>
+    <q-dialog
+      v-model="opened_add_tag_modal"
+    >
+      <q-card class="q-mb-md" style="min-width: 40vh !important;">
+        <q-card-section>
+          <div class="col-md-6 col-sm-6 col-xs-12 q-pa-sm">
+            <q-card class="q-mb-md">
+              <q-card-section style="color: #35495E">
+                <div
+                  class="row items-center justify-between"
+                  style="font-size: 16px; text-transform: uppercase"
+                >
+                  Tags
+                </div>
+              </q-card-section>
+            </q-card>
+            <q-card class="q-mb-md q-pa-md">
+              <q-input
+                v-model="tag.name"
+                class="q-ml-sm"
+                clearable
+                label="Add tag"
+                color="green"
+              >
+                <template v-slot:after>
+                  <q-btn
+                    class="bg-green"
+                    round
+                    dense
+                    flat
+                    text-color="white"
+                    icon="fas fa-plus"
+                    @click="!tag.uuid ? addTag() : updateTag()"
+                  />
+                </template>
+              </q-input>
+            </q-card>
+            <q-card class="q-mb-md" style="height: 33vh">
+              <q-scroll-area style="height: 100%;">
+                <q-card-section>
+                  <div
+                    v-for="tag in getRepositoryTags(tag.repository_id)"
+                    v-bind:key="tag.uuid"
+                  >
+                    <q-item>
+                      <q-item-section>
+                        {{ tag.name }}
+                      </q-item-section>
+                      <q-item-section side>
+                        <q-btn
+                          round
+                          size="sm"
+                          class="bg-purple"
+                          text-color="white"
+                          icon="fas fa-edit"
+                          @click="editTagName(tag)"
+                        />
+                      </q-item-section>
+                      <q-item-section side>
+                        <q-btn
+                          round
+                          size="sm"
+                          class="bg-red"
+                          text-color="white"
+                          icon="fas fa-trash-alt"
+                          @click="removeTag(tag.uuid)"
+                        />
+                      </q-item-section>
+                    </q-item>
+                  </div>
+                </q-card-section>
+              </q-scroll-area>
+            </q-card>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -76,11 +186,17 @@ export default {
   methods: {
     async getStore() {
       await this.getUser();
+      await this.getTags();
       this.getRepositories();
     },
     async getUser() {
       if (!this.user.login && !this.$store.state.users.loading_users) {
         await this.$store.dispatch('users/get');
+      }
+    },
+    async getTags() {
+      if (this.tags.length <= 0) {
+        await this.$store.dispatch('tags/getAll');
       }
     },
     getRepositories() {
@@ -91,13 +207,49 @@ export default {
     openRepository(url) {
       openURL(url);
     },
+    showTagField(repository_id) {
+      this.tag.repository_id = repository_id;
+      this.opened_add_tag_modal = true;
+    },
+    editTagName(tag) {
+      const { uuid, name, repository_id } = tag;
+      this.tag = { uuid, name, repository_id };
+    },
+    addTag() {
+      this.$store.dispatch('tags/create', this.tag);
+
+      this.clearTag();
+    },
+    updateTag() {
+      this.$store.dispatch('tags/update', this.tag);
+
+      this.clearTag();
+    },
+    removeTag(uuid) {
+      this.$store.dispatch('tags/remove', uuid);
+    },
+    getRepositoryTags(repository_id) {
+      return this.tags.filter((tag) => tag.repository_id === repository_id);
+    },
+    clearTag() {
+      this.tag.uuid = null;
+      this.tag.name = null;
+    },
   },
   computed: {
     user() {
       return this.$store.state.users.user;
     },
+    tags() {
+      return this.$store.state.tags.tags;
+    },
     repositories() {
-      return this.$store.state.repositories.repositories;
+      return this.$store.state.repositories.repositories
+        .filter((repository) => (this.tags
+          .filter((tag) => tag.name.toLowerCase()
+            .includes(this.filterTag.toLowerCase()))
+          .map((tag) => tag.repository_id))
+          .includes(repository.id));
     },
   },
 };
